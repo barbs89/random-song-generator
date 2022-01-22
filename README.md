3501432dc4805528c8695776927ef4327d23f4c4
Barbara Eve Rodriguez <Eve@barbaras-mbp.lan>
2022-01-23 10:59:17 +1100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Random Song Generator
+
+## Project Objectives:
+- Build a message generator program using JavaScript
+- Use Git version control
+- Use command line
+- Develop locally on your computer
---
+++ randomSongGenerator.js
@@ -0,0 +1,13 @@
+const songs = ["Hopelessly Devoted to You","Take My Breath Away","She’s Like the Wind","I Will Always Love You","What a Feeling"]
+const artists = ["Olivia Newton-John", "Berlin","Patrick Swayze","Whitney Houston","Irene Cara"]
+const movies = ["Grease","Top Gun", "Dirty Dancing","The Bodyguard","Flashdance"]
+
+let scrambleSong = Math.floor(Math.random() * (songs.length))
+let scrambleArtist = Math.floor(Math.random() * (artists.length))
+let scrambleMovie = Math.floor(Math.random() * (movies.length))
+
+const randomChoice = (song,artist,movie) => {
+    return "\""  + songs[song] + "\" by " + artists[artist] + ' - ' + movies[movie] + '.';
+}
+
+console.log(randomChoice(scrambleSong,scrambleArtist,scrambleMovie))
