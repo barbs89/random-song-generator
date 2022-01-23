5564c3a6cebb0acc4e49263e747f705ab1f5993f
Barbara Eve Rodriguez <Eve@barbaras-mbp.lan>
2022-01-23 11:51:12 +1100

    Remove variables, add scramble function

--- randomSongGenerator.js
+++ randomSongGenerator.js
@@ -2,12 +2,14 @@ const songs = ["Hopelessly Devoted to You","Take My Breath Away","She’s Like t
 const artists = ["Olivia Newton-John", "Berlin","Patrick Swayze","Whitney Houston","Irene Cara"]
 const movies = ["Grease","Top Gun", "Dirty Dancing","The Bodyguard","Flashdance"]
 
-let scrambleSong = Math.floor(Math.random() * (songs.length))
-let scrambleArtist = Math.floor(Math.random() * (artists.length))
-let scrambleMovie = Math.floor(Math.random() * (movies.length))
+// let scrambleSong = Math.floor(Math.random() * (songs.length))
+// let scrambleArtist = Math.floor(Math.random() * (artists.length))
+// let scrambleMovie = Math.floor(Math.random() * (movies.length))
+
+const scramble = arr => Math.floor(Math.random() * (arr.length));
 
 const randomChoice = (song,artist,movie) => {
     return "\""  + songs[song] + "\" by " + artists[artist] + ' - ' + movies[movie] + '.';
 }
 
-console.log(randomChoice(scrambleSong,scrambleArtist,scrambleMovie))
+console.log(randomChoice(scramble(songs),scramble(artists),scramble(movies)))
