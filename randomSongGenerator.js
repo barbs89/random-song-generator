d7b56dae0f65c960c9cb99d442038521e19de65e
Barbara Eve Rodriguez <Eve@barbaras-mbp.lan>
2022-01-23 11:51:51 +1100

    Remove unused code

--- randomSongGenerator.js
+++ randomSongGenerator.js
@@ -2,10 +2,6 @@ const songs = ["Hopelessly Devoted to You","Take My Breath Away","She’s Like t
 const artists = ["Olivia Newton-John", "Berlin","Patrick Swayze","Whitney Houston","Irene Cara"]
 const movies = ["Grease","Top Gun", "Dirty Dancing","The Bodyguard","Flashdance"]
 
-// let scrambleSong = Math.floor(Math.random() * (songs.length))
-// let scrambleArtist = Math.floor(Math.random() * (artists.length))
-// let scrambleMovie = Math.floor(Math.random() * (movies.length))
-
 const scramble = arr => Math.floor(Math.random() * (arr.length));
 
 const randomChoice = (song,artist,movie) => {
